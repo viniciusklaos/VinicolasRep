ce30b08e65dade1f6a3b22fa4a4e73a608ca9c32
KlaosVK <vitalhost11@gmail.com>
2017-12-04 13:13:14 -0200

    Verificação WHMCONFIG
    Se não encontrar, EMAIL-ME.

--- super/super_automatico_funcoes.sh
+++ super/super_automatico_funcoes.sh
@@ -94,16 +94,56 @@ gera_spf () {
 }
 whm_config () {
 	# - Fazendo as configurações do "Basic"
-	sed -i '/ADDR /d' /etc/wwwacct.conf
-	sed -i "/NSTTL /i ADDR $ipprincipal" /etc/wwwacct.conf
-	sed -i '/NS /d' /etc/wwwacct.conf
-	sed -i "/HOMEDIR /i NS ns1.$dominio" /etc/wwwacct.conf
-	sed -i '/CONTACTEMAIL /d' /etc/wwwacct.conf
-	sed -i "/HOST /i CONTACTEMAIL root@server.$dominio" /etc/wwwacct.conf
-	sed -i '/NS2 /d' /etc/wwwacct.conf
-	sed -i "/HOMEMATCH /i NS2 ns2.$dominio" /etc/wwwacct.conf
-	sed -i '/HOST /d' /etc/wwwacct.conf
-	sed -i "/DEFWEBMAILTHEME /i HOST server.$dominio" /etc/wwwacct.conf
+	if grep "ADDR " /etc/wwwacct.conf; then
+	  if grep "ADDR $ipprincipal" /etc/wwwacct.conf; then
+	    sleep 0
+	  else
+	    sed -i '/ADDR /d' /etc/wwwacct.conf
+	    echo "ADDR $ipprincipal" >> /etc/wwwacct.conf
+	  fi
+	else
+		   echo "Nao encontrado ADDR no servidor: $dominio" | mail -s "ADDR FAIL $dominio" vinicius@centraldousuario.com.br
+	fi
+	if grep "NS " /etc/wwwacct.conf; then
+	  if grep "NS ns1.$dominio" /etc/wwwacct.conf; then
+	    sleep 0
+	  else
+	    sed -i '/NS /d' /etc/wwwacct.conf
+	    echo "NS ns1.$dominio" >> /etc/wwwacct.conf
+	  fi
+	else
+	  echo "Nao encontrado NS no servidor: $dominio" | mail -s "NS FAIL $dominio" vinicius@centraldousuario.com.br
+	fi
+	if grep "CONTACTEMAIL " /etc/wwwacct.conf; then
+	  if grep "CONTACTEMAIL root@server.$dominio" /etc/wwwacct.conf; then
+	    sleep 0
+	  else
+	    sed -i '/CONTACTEMAIL /d' /etc/wwwacct.conf
+	    echo "CONTACTEMAIL root@server.$dominio" >> /etc/wwwacct.conf
+	  fi
+	else
+	  echo "Nao encontrado CONTACTEMAIL no servidor: $dominio" | mail -s "CONTACTEMAIL FAIL $dominio" vinicius@centraldousuario.com.br
+	fi
+	if grep "NS2 " /etc/wwwacct.conf; then
+	  if grep "NS2 ns2.$dominio" /etc/wwwacct.conf; then
+	    sleep 0
+	  else
+	    sed -i '/NS2 /d' /etc/wwwacct.conf
+	    echo "NS2 ns2.$dominio" >> /etc/wwwacct.conf
+	  fi
+	else
+	  echo "Nao encontrado NS2 no servidor: $dominio" | mail -s "NS2 FAIL $dominio" vinicius@centraldousuario.com.br
+	fi
+	if grep "HOST " /etc/wwwacct.conf; then
+	  if grep "HOST server.$dominio" /etc/wwwacct.conf; then
+	    sleep 0
+	  else
+	    sed -i '/HOST /d' /etc/wwwacct.conf
+	    echo "HOST server.$dominio" >> /etc/wwwacct.conf
+	  fi
+	else
+	  echo "Nao encontrado HOST no servidor: $dominio" | mail -s "HOST FAIL $dominio" vinicius@centraldousuario.com.br
+	fi
 	# - Desabilitando o Ipv6
 	perl -p -i -e 's/NETWORKING_IPV6="yes"/NETWORKING_IPV6="no"/g' /etc/sysconfig/network
 	perl -p -i -e 's/IPV6INIT="yes"/IPV6INIT="no"/g' /etc/sysconfig/network-scripts/*
