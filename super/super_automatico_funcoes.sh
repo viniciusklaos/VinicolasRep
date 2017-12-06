d10371af1f674042c716f98c6cfe4a56d11bcb63
KlaosVK <vitalhost11@gmail.com>
2017-12-06 13:53:59 -0200

    PHP Corrigido.

--- super/super_automatico_funcoes.sh
+++ super/super_automatico_funcoes.sh
@@ -285,36 +285,15 @@ configura_exim () {
 	perl -p -i -e 's/smtp_accept_max = 100/smtp_accept_max = 5000/g' /etc/exim.conf
 }
 configura_php () {
-	# - Configurando o PHP 5.5
-	perl -p -i -e 's/max_execution_time = 30/max_execution_time = 300/g' /opt/cpanel/ea-php55/root/etc/php.d/local.ini
-	perl -p -i -e 's/max_input_time = 60/max_input_time = 300/g' /opt/cpanel/ea-php55/root/etc/php.d/local.ini
-	perl -p -i -e 's/upload_max_filesize = 2M/upload_max_filesize = 8M/g' /opt/cpanel/ea-php55/root/etc/php.d/local.ini
-	perl -p -i -e 's/vmemory_limit = 64M/memory_limit = 1024M/g' /opt/cpanel/ea-php55/root/etc/php.d/local.ini
-	#
-	perl -p -i -e 's/max_execution_time = 30/max_execution_time = 300/g' /opt/cpanel/ea-php55/root/etc/php.ini
-	perl -p -i -e 's/max_input_time = 60/max_input_time = 300/g' /opt/cpanel/ea-php55/root/etc/php.ini
-	perl -p -i -e 's/upload_max_filesize = 2M/upload_max_filesize = 8M/g' /opt/cpanel/ea-php55/root/etc/php.ini
-	perl -p -i -e 's/vmemory_limit = 64M/memory_limit = 1024M/g' /opt/cpanel/ea-php55/root/etc/php.ini
-	# - Configurando o PHP 5.6
-	perl -p -i -e 's/max_execution_time = 30/max_execution_time = 300/g' /opt/cpanel/ea-php56/root/etc/php.d/local.ini
-	perl -p -i -e 's/max_input_time = 60/max_input_time = 300/g' /opt/cpanel/ea-php56/root/etc/php.d/local.ini
-	perl -p -i -e 's/upload_max_filesize = 2M/upload_max_filesize = 8M/g' /opt/cpanel/ea-php56/root/etc/php.d/local.ini
-	perl -p -i -e 's/vmemory_limit = 64M/memory_limit = 1024M/g' /opt/cpanel/ea-php56/root/etc/php.d/local.ini
-	#
-	perl -p -i -e 's/max_execution_time = 30/max_execution_time = 300/g' /opt/cpanel/ea-php56/root/etc/php.ini
-	perl -p -i -e 's/max_input_time = 60/max_input_time = 300/g' /opt/cpanel/ea-php56/root/etc/php.ini
-	perl -p -i -e 's/upload_max_filesize = 2M/upload_max_filesize = 8M/g' /opt/cpanel/ea-php56/root/etc/php.ini
-	perl -p -i -e 's/vmemory_limit = 64M/memory_limit = 1024M/g' /opt/cpanel/ea-php56/root/etc/php.ini
-	# - Configurando o PHP 7.0
-	perl -p -i -e 's/max_execution_time = 30/max_execution_time = 300/g' /opt/cpanel/ea-php70/root/etc/php.d/local.ini
-	perl -p -i -e 's/max_input_time = 60/max_input_time = 300/g' /opt/cpanel/ea-php70/root/etc/php.d/local.ini
-	perl -p -i -e 's/upload_max_filesize = 2M/upload_max_filesize = 8M/g' /opt/cpanel/ea-php70/root/etc/php.d/local.ini
-	perl -p -i -e 's/vmemory_limit = 64M/memory_limit = 1024M/g' /opt/cpanel/ea-php70/root/etc/php.d/local.ini
-	#
-	perl -p -i -e 's/max_execution_time = 30/max_execution_time = 300/g' /opt/cpanel/ea-php70/root/etc/php.ini
-	perl -p -i -e 's/max_input_time = 60/max_input_time = 300/g' /opt/cpanel/ea-php70/root/etc/php.ini
-	perl -p -i -e 's/upload_max_filesize = 2M/upload_max_filesize = 8M/g' /opt/cpanel/ea-php70/root/etc/php.ini
-	perl -p -i -e 's/vmemory_limit = 64M/memory_limit = 1024M/g' /opt/cpanel/ea-php70/root/etc/php.ini
+	PHP56="/opt/cpanel/ea-php56/root/etc/php.ini"
+	sed -i '/max_execution_time =/d' $PHP56
+	sed -i '/max_input_time =/d' $PHP56
+	sed -i '/upload_max_filesize =/d' $PHP56
+	sed -i '/memory_limit =/d' $PHP56
+	echo "max_execution_time = 300" >> $PHP56
+	echo "max_input_time = 300" >> $PHP56
+	echo "upload_max_filesize = 8M" >> $PHP56
+	echo "memory_limit = 1024M" >> $PHP56
 }
 motd () {
 	# - MOTD
